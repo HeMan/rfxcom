e2459ea6a011b4bb5ad587977b724cdb4dd38bfb
Jimmy Hedman <jimmy.hedman@southpole.se>
2012-09-18 23:42:10 +0200

    Some code clean up.
    - Added bitwise operations instead of ugly (non-working) workarounds.
    - Added function to parse data that allways is the same.
    - Added some luadoc.

--- parse.lua
+++ parse.lua
@@ -1,4 +1,4 @@
---
+---
 --------------------------------------------------------------------------------
 --         FILE:  parse.lua
 --        USAGE:  require "parse"
@@ -15,44 +15,68 @@
 --------------------------------------------------------------------------------
 --
 
+local bit = require "bit"
 local M = {}
 local parsers = {}
+
+--- Function to convert indata from binary "string" to table of values
+-- First byte is datatype
+-- Rest of bytes is binary data from the RFXcom USB tranciever
+-- @param indata the binary "string" from RFXcom
+-- @return pair of data consisting of datatype and table with data
+
 function tableconvert(indata)
 	local datatype = string.byte(indata:sub(1,1))
 	local t = { indata:byte(2, #indata) }
 	return datatype,t
 end
 
-parsers[17]=function(indata)
+function parsesome(indata, subtypes, idbytes)
 	local t = {}
-	local subtypes = { [0]='AC', 'HomeEasy EU', 'ANSLUT' }
-	local commands = { [0]='Off', 'On', 'Set leve', 'Group off', 'Group on','Set group level' }
+	local bytes = 0
+	local id = 0
 	t['subtype']=indata[1]
 	t['subname']=subtypes[indata[1]]
 	t['seqnr']=indata[2]
-	t['id']=indata[3]*16777216+indata[4]*65535+indata[5]*256+indata[6]
-	t['unitcode']=indata[7]
-	t['cmnd']=indata[9]
-	t['command']=commands[indata[8]]
-	t['level']=indata[9]
-	t['rssi']=indata[10]
+	while (bytes < idbytes) do
+		print(id, bytes)
+		id=bit.lshift(id,8)+indata[3+bytes]
+		bytes=bytes+1
+	end
+	t['id']=id
 	return t
 end
 
-parsers[80]=function(indata)
+parsers[0x11]=function(indata)
+	local t = {}
+	local subtypes = { [0]='AC', 'HomeEasy EU', 'ANSLUT' }
+	local commands = { [0]='Off', 'On', 'Set leve', 'Group off', 'Group on','Set group level' }
+	t=parsesome(indata, subtypes, 4)
+
+	t['unitcode']=indata[7]
+	t['cmnd']=indata[8]
+	t['command']=commands[indata[8]]
+	t['level']=indata[9]
+	t['rssi']=bit.band(indata[10],0x0F)
+	return t
+end
+
+parsers[0x50]=function(indata)
 	local t = {}
 	local subtypes = {"THR128/138, THC138", 
 	"THC238/268,THN132,THWR288,THRN122,THN122,AW129/131",
 	"THWR800","RTHN318","La Crosse TX3, TX4, TX17",
-	"TS15C","Viking 02811"}
-	t['subtype']=indata[1]
-	t['subname']=subtypes[indata[1]]
-	t['seqnr']=indata[2]
-	t['id']=indata[3]*256+indata[4]
-	t['temp']=((indata[5])*256+indata[6])/10
+	"TS15C","Viking 02811","La Crosse WS2300","RUBiCSON",
+	"TFA 30.3133"}
+	t=parsesome(indata,subtypes,2)
+
+	t['temp']=((bit.band(indata[5],0x7F))*256+indata[6])/10
+	if (bit.band(indata[5],0x80)==0x80) then
+		t['temp']=-t['temp']
+	end
 	t['tempraw']=indata[5]*256+indata[6]
-	t['battery']=(indata[7])/16
-	t['rssi']=indata[7]
+	t['battery']=bit.rshift(indata[7],4)
+	t['rssi']=bit.band(indata[7],15)
 
 	return t
 end
