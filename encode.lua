d820e84b6f521f0e19e03c9cb2275fefd4921ae2
Jimmy Hedman <jimmy.hedman@southpole.se>
2012-09-30 09:51:35 +0200

    Added function to controll LIGHTNING2.
    - Code can now controll AC, HomeEasy EU and ANSLUT devices.

--- encode.lua
+++ encode.lua
@@ -14,10 +14,25 @@
 --     REVISION:  ---
 --------------------------------------------------------------------------------
 --
+local bit = require "bit"
+
+require "common"
 
 local M = {}
 
 local encoders = {}
+
+function splitid(id, idbytes)
+	local bytes = 0
+	local idstring = ''
+	while bytes < idbytes do
+		print(id)
+		idstring = string.char(bit.band(id, 0xFF))..idstring
+		id = bit.rshift(id, 8)
+		bytes = bytes + 1
+	end
+	return idstring
+end
 
 function M.reset()
 	return string.char(0,0,0,0,0,0,0,0,0,0,0,0,0)
@@ -28,9 +43,14 @@ function M.get_status()
 	return string.char(0,0,1,2,0,0,0,0,0,0,0,0,0)
 end  ----------  end of function get_status  ----------
 
+encoders[LIGHTNING2] = function(subtype, id, unitcode, command)
+	return string.char(LIGHTNING2, subtype, 0)..splitid(id, 4)..string.char(unitcode, command, 0, 0)
+end
 
 function M.encode(package)
 	return string.char(string.len(package))..package
 end  ----------  end of function M.encode  ----------
+
+M.encoders = encoders
 return M
 
